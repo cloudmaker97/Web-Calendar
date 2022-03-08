ee1e9396ec6dd2782a2bf9789fcd87463f172ce0
Dennis Heinrich <d.heinrich@wnm.de>
2022-03-08 08:51:34 +0100

    PHP Coding Kata - ASCII Kalender

---
+++ index.php
@@ -0,0 +1,139 @@
+<?php
+/* Coding Kata | Dennis H. */
+const DAYS_IN_WEEK = 7;
+$args = $argv;
+
+// Holt sich ein Array der Wochentage oder gibt den spezifischen Wert des Index aus
+function GetDaysShort($index=-1) {
+	$days = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"];
+	if($index == -1) {
+		return $days;
+	} else {
+		return $days[$index];
+	}
+}
+
+// Holt sich ein Array der Monatsnamen oder gibt den spezifischen Wert des Index aus
+function GetMonth($index=-1) {
+	$index = $index-1;
+	$days = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+	if($index == -1) {
+		return $days;
+	} else {
+		return $days[$index];
+	}
+}
+
+// Holt die Anzahl der Tage im Gregoranischen Kalender zum angegebenen Datum
+function GetDaysOfMonth() {
+	$yearAndMonth = GetYearAndMonth();
+	return cal_days_in_month(CAL_GREGORIAN, $yearAndMonth[1], $yearAndMonth[0]);
+}
+
+// Holt den ersten Wochentag (1-7, 1 = Montag) zum angegebenen Datum
+function FirstWeekday() {
+	$yearAndMonth = GetYearAndMonth();
+	return date('N', mktime(0, 0, 0, $yearAndMonth[1], 1, $yearAndMonth[0])); 
+}
+
+// Holt Jahr und Monat aus den Programm-Argumenten oder nimmt das aktuelle
+function GetYearAndMonth() {
+	global $args;
+	$month = null;
+	$year = null;
+	if(isset($args[1]) && isset($args[2])) {
+		$month = $args[1];
+		$year = $args[2];
+	}
+	if($year == null && $month == null) {
+		if(isset($args[1])) {
+			throw new Exception("Argumente unvollständig, es wurde kein Jahr angegeben");
+		}	
+
+		$time = new DateTime();
+		return [(int)$time->format("Y"), (int)$time->format("m")];
+	} else {
+		return [(int)$year, (int)$month];
+	}
+}
+
+// Prüft ob das Datum mit dem aktuellen angezeigten Kalender übereinstimmt
+function GetIsCurrentCalendar() {
+	$time = new DateTime();
+	$currentTimeArr = [$time->format("Y"), $time->format("m")];
+	$selectedTimeArr = GetYearAndMonth();
+	return ($currentTimeArr[0] == $selectedTimeArr[0] && $currentTimeArr[1] == $selectedTimeArr[1]);
+}
+
+// Hole den aktuellen Tag des jetzigen Monats
+function GetCurrentDayOfMonth() {
+	$time = new DateTime();
+	return $time->format("d");
+}
+
+// Erstellt den Kalender und gibt diesen aus
+function BuildCalendar() {
+	$yearAndMonth = GetYearAndMonth();
+	$daysInMonth = GetDaysOfMonth();
+	$firstWeekday = FirstWeekday();
+	$currentDayOfMonth = GetCurrentDayOfMonth();
+	$isCurrentCalendar = GetIsCurrentCalendar();
+	$monthOfCalendar = GetMonth($yearAndMonth[1]);
+	echo "\r\n\t";
+	echo sprintf("== %s, %s ==\r\n", $monthOfCalendar, $yearAndMonth[0]);
+
+	echo "\r\n";
+	foreach(GetDaysShort() as $Day) {
+		echo "\t".$Day;
+	}
+
+	echo "\r\n\t- - - - - - - - - - - - - - - - - - - - - - - - - - - - \r\n\r\n";
+	$offsetDays = $firstWeekday-1;
+	for ($x=0; $x < $offsetDays; $x++) { 
+		echo " "."\t";
+	}
+
+	for ($i=1; $i <= $daysInMonth; $i++) { 
+		echo "\t";
+		if($i == $currentDayOfMonth && $isCurrentCalendar) {
+			echo sprintf("(%s)", $i);
+		} else {
+			echo $i;
+		}
+		
+		if(($i+$offsetDays) % DAYS_IN_WEEK == 0) {
+			echo "\r\n\r\n";
+		}
+	}
+
+	echo "\r\n";
+}
+
+// Prüfe ob die Benutzereingaben valide sind
+function CheckValidUserInput() {
+	$valid = true;
+
+	$yearAndMonth = GetYearAndMonth();
+	$year = $yearAndMonth[0];
+	$month = $yearAndMonth[1];
+
+	if($month < 1 || $month > 12) {
+		$valid = false;
+	}
+
+	if($year < 0 || $year > 9999) {
+		$valid = false;
+	}
+
+	return $valid;
+}
+
+try {
+	if(CheckValidUserInput()) {
+		BuildCalendar();
+	} else {
+		echo "Die Benutzereingaben stimmen nicht; index.php {Monat|00} {Jahr|0000}";
+	}
+} catch(Exception $e) {
+	echo "Fehler: ".$e->getMessage();
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# PHP Kalender (ASCII)
+
+Dieses Script kann einen Kalender auf einer Webseite oder einer Eingabeaufforderung darstellen. Das Script ist in der Lage auch auf Wunsch einen anderen Kalenderbereich mit Angabe von Monat und Jahr (XX XXXX) anzuzeigen, z.B. "php index.php 3 2022" zeigt den Monat März des Jahres 2022 an. Ist kein Datum angegeben, so wird der aktuelle Kalendertag geklammert dargestellt. 
